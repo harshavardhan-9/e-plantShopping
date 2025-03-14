ac1f60acdd3cc4b1668d3016522f75c27097b161
Harsha <vardhanharsha607@gmail.com>
2025-03-14 15:54:43 -0400

    BugFix

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -46,13 +46,7 @@ const CartItem = ({ onContinueShopping }) => {
     const handleDecrement = (item) => {
         const updatedItem = { ...item };
 
-        if (updatedItem.quantity == 1) {
-            // Remove item if number of items gets decremented to 0
-            dispatch(removeItem(updatedItem));
-        } else {
-            updatedItem.quantity--;
-            dispatch(updateQuantity(updatedItem));
-        }
+       
     };
 
     const handleRemove = (item) => {
